f7c6cd58e3e59c8583b1132358b890f37e9f0412
sofijacom <107557749+sofijacom@users.noreply.github.com>
2026-07-19 22:26:24 +0300

    Update

--- common/hooks/pre-configure/00-cross-host.sh
+++ common/hooks/pre-configure/00-cross-host.sh
@@ -2,5 +2,6 @@
 # extra/common/hooks/pre-configure/00-cross-host.sh
 
 if [ -n "$XBPS_CROSS_BUILD" ]; then
-    export CONFIGURE_ARGS="--host=$XBPS_TARGET_MACHINE-linux-gnu $CONFIGURE_ARGS"
+  # Просто добавляем флаг. xbps-src подхватит CONFIGURE_ARGS автоматически
+  CONFIGURE_ARGS="--host=$XBPS_TARGET_MACHINE-linux-gnu $CONFIGURE_ARGS"
 fi
