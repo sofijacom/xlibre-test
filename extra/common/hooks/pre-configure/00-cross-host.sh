c684ef0926c623a7f65612d86e7df1254eae05be
sofijacom <107557749+sofijacom@users.noreply.github.com>
2026-07-19 21:14:42 +0300

    Update

--- extra/common/hooks/pre-configure/00-cross-host.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# extra/common/hooks/pre-configure/00-cross-host.sh
-
-if [ -n "$XBPS_CROSS_BUILD" ]; then
-    export CONFIGURE_ARGS="--host=$XBPS_TARGET_MACHINE-linux-gnu $CONFIGURE_ARGS"
-fi
